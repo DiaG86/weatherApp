bc49e063b104e38dcb452a27d370d6647c532045
DiaG86 <adia.grigsby@gmail.com>
2020-05-30 19:37:35 -0400

    addedd gradient,added cards to html,api key error

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,21 @@
 $("#citySearch").on("click", function(event) {
 
-var queryURL ="http://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={e9561b7f2c048c896aca004a3ef59384}"
+var queryURL ="http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},+'&count='&appid={e9561b7f2c048c896aca004a3ef59384}"
+var city ="your city"
+var key= "e9561b7f2c048c896aca004a3ef59384"
 
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
+    "url": "queryURL",
+    "method": "GET",
+    "dataType": "json",
+    // "data": {
+    //     "q": "city",
+    //     "appid": "key",
+    //     "units": "imperial",
+    //     "cnt": "5"
 })
+
 .then(function(response) {
     console.log(response)
 })
