052efcf1ec0bc07a4fd9ba95f6b1104e1385990a
DiaG86 <adia.grigsby@gmail.com>
2020-05-30 17:46:51 -0400

    added ajax and console.log the response

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={f2cf01533a240f4102f437a7903f9e8c}"
+$("#citySearch").on("click", function(event) {
 
+var queryURL ="http://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={e9561b7f2c048c896aca004a3ef59384}"
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+.then(function(response) {
+    console.log(response)
+})
+});
