78ef0de4e80c3b6b695eadcb3c8d7e8e61b6f8f2
DiaG86 <adia.grigsby@gmail.com>
2020-05-30 22:02:51 -0400

    added variables, set to render cities

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,29 @@
-$("#citySearch").on("click", function(event) {
+var cityInput = document.querySelector("#city-text");
+var cardBody = document.querySelector("#card-body");
+var cityList = document.querySelector("#city-list");
+var cities =["Charlotte"]
+
+renderCities();
+
+function renderCities() {
+  // Clear cityList element
+  cityList.innerHTML = "";
+  
+
+  // Render a new li for each city
+  for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
+
+    var li = document.createElement("li");
+    li.textContent = cities;
+    li.setAttribute("data-index", i);
+
+    li.appendChild(button);
+    cityList.appendChild(li);
+  }
+}
+
+$("#city-text").on("click", function(event) {
 
     var APIKey= "e9561b7f2c048c896aca004a3ef59384"
     var queryURL ="http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},&appid=" + APIKey;
@@ -14,6 +39,21 @@ $.ajax({
     console.log(response);
     // Log the queryURL
     console.log(queryURL);
+
+    var cityText = cityInput.value.trim();
+      
+    // Return from function early if submitted todoText is blank
+    if (cityText === "") {
+        return;
+      }
+    
+      // Add new todoText to todos array, clear the input
+      cities.push(cityText);
+      cityInput.value = "";
+    
+      // Re-render the list
+      renderTodos();
+   
 
  // Transfer content to HTML
  $(".city").html("<h1>" + response.name + " Weather Details</h1>");
@@ -31,4 +71,7 @@ $.ajax({
         console.log("Temperature (F): " + tempF);
 
 })
+
 });
+
+
