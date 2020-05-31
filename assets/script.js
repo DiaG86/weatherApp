5ad335612974d0d0831c41aff4024a80b274eeb8
DiaG86 <adia.grigsby@gmail.com>
2020-05-30 21:13:12 -0400

    added temparature conversion and text options

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,34 @@
 $("#citySearch").on("click", function(event) {
 
-var queryURL ="http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},+'&count='&appid={e9561b7f2c048c896aca004a3ef59384}"
-var city ="your city"
-var key= "e9561b7f2c048c896aca004a3ef59384"
+    var APIKey= "e9561b7f2c048c896aca004a3ef59384"
+    var queryURL ="http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},&appid=" + APIKey;
+
 
 
 $.ajax({
-    "url": "queryURL",
-    "method": "GET",
-    "dataType": "json",
-    // "data": {
-    //     "q": "city",
-    //     "appid": "key",
-    //     "units": "imperial",
-    //     "cnt": "5"
-})
+    url: queryURL,
+    method: "GET"
+  })
 
 .then(function(response) {
-    console.log(response)
+    console.log(response);
+    // Log the queryURL
+    console.log(queryURL);
+
+ // Transfer content to HTML
+ $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+ $(".wind").text("Wind Speed: " + response.wind.speed);
+ $(".humidity").text("Humidity: " + response.main.humidity);
+ 
+ // Convert the temp to fahrenheit
+ var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+ $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + tempF);
+
 })
 });
